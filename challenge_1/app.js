6400f55a704956095ec0147735125ad460834f18
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-20 15:53:21 -0700

    Add event listeners to each cell

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,62 @@
+const cells = document.getElementsByTagName('td');
+console.log(cells);
+
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', (e) => console.log('Clicked!', e));
+};
+
+// cells.forEach(cell => cell.addEventListener('click', (e) => console.log('Clicked!', e)));
 
 
+
+
+
+// class Board {
+//   constructor() {
+//     this.boardValues = [
+//       [null, null, null],
+//       [null, null, null],
+//       [null, null, null]
+//     ];
+//     this.table = document.createElement('table');
+//     this.row = document.createElement('tr');
+//     this.cell = document.createElement('td');
+//   }
+
+
+
+// }
+
+
+// let table = document.createElement('table');
+// let body = document.getElementById('root');
+
+
+// // Add buttons to table
+// let cell = document.createElement('td');
+
+
+
+
+/* VIEW */
+// Render Table to DOM
+// const createNewBoard = () => {
+//   let body = document.getElementById('root');
+//   let table = document.getElementsByTagName('table');
+
+//   clearBoard(body);
+
+//   let newBoard = new Board();
+//   body.appendChild(newBoard.table);
+//   console.log('New board created!');
+// }
+
+// const clearBoard = (board) => {
+//   console.log('Clearing board');
+//   board.innerHTML = "";
+// }
+
+// //INITIALIZE
+// window.onload = () => {
+//   createNewBoard();
+// }
