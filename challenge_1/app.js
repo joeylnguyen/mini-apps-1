21994aef77f3b794357cad88b727e1d7eddcc485
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-20 23:07:31 -0700

    Finish bare minimum requirements
    - Scores update dynamically
    - Win message toggles when there's a win or tie

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,5 @@
 // MODEL
-let turnChecker = false;
+let turnChecker = 0;
 
 let player1Score = 0;
 let player2Score = 0;
@@ -12,14 +12,47 @@ let boardData = [
 ];
 
 const fillBoardData = (row, column, value) => {
-  console.log(boardData);
   boardData[row][column] = value;
 }
 
-//VIEW
+const checkWin = () => {
+    let majorDiag = boardData[0][0] + boardData[1][1] + boardData[2][2];
+    let minorDiag = boardData[0][2] + boardData[1][1] + boardData[2][0];
+    let row1 = boardData[0][0] + boardData[0][1] + boardData[0][2];
+    let row2 = boardData[1][0] + boardData[1][1] + boardData[1][2];
+    let row3 = boardData[2][0] + boardData[2][1] + boardData[2][2];
+    let col1 = boardData[0][0] + boardData[1][0] + boardData[2][0];
+    let col2 = boardData[0][1] + boardData[1][1] + boardData[2][1];
+    let col3 = boardData[0][2] + boardData[1][2] + boardData[2][2];
 
+    let winCombos = [majorDiag, minorDiag, row1, row2, row3, col1, col2, col3];
+
+    for (let i = 0; i <= winCombos.length; i++) {
+      if (winCombos[i] === 3) {
+        player1Score++;
+        toggleWin('Player 1 wins!', 'block');
+        break;
+      } else if (winCombos[i] === -3) {
+        player2Score++;
+        toggleWin('Player 2 wins!', 'block');
+        break;
+      } else if (turnChecker === 9) {
+        tieScore++;
+        toggleWin('Tie!', 'block');
+        break;
+      }
+    }
+    toggleScore();
+  }
+
+
+//VIEW
 const rows = document.getElementsByTagName('tr');
 const playAgainButton = document.getElementById('playAgain');
+const winMessage = document.getElementById('winner');
+const p1ScoreDisplay = document.getElementById('player1Score');
+const p2ScoreDisplay = document.getElementById('player2Score');
+const tieScoreDisplay = document.getElementById('tieScore')
 
 playAgainButton.addEventListener('click', (e) => handlePlayAgainButtonClick(e));
 
@@ -38,31 +71,48 @@ const createNewBoard = () => {
 }
 
 const clearBoard = () => {
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].innerHTML = "";
+  for (var i = 0; i < rows.length; i++) {
+    let cells = rows[i].children;
+    for (var j = 0; j < cells.length; j++) {
+      cells[j].innerHTML = "";
+      boardData[i][j] = null;
+    }
   }
 }
 
-const toggleSymbol = (cell, turn) => (turn ? cell.innerHTML = 'X' : cell.innerHTML = 'O')
+const toggleScore = () => {
+  p1ScoreDisplay.innerHTML = `Player 1 Score: ${player1Score}`;
+  p2ScoreDisplay.innerHTML = `Player 2 Score: ${player2Score}`;
+  tieScoreDisplay.innerHTML = `Tie: ${tieScore}`;
+}
+
+const toggleSymbol = (cell, turn) => (turn ? cell.innerHTML = 'X' : cell.innerHTML = 'O');
+
+const toggleWin = (player, state) => {
+  winMessage.innerHTML = player;
+  winMessage.style.fontSize = '50px';
+  winMessage.style.display = state;
+}
 
 // CONTROLLER
 const handlePlayAgainButtonClick = (event) => {
   clearBoard();
-  turnChecker = false;
-
+  turnChecker = 0;
+  toggleWin('Nah', 'none')
 }
 
 const handleCellClick = (event) => {
   let {row, column} = event.target.value;
-
-  turnChecker = !turnChecker;
+  let turn = turnChecker % 2 === 0;
   let value;
-  turnChecker ? value = 1 : value = 0;
+
+  turn ? value = 1 : value = -1;
+  turnChecker++;
 
   fillBoardData(row, column, value);
-  toggleSymbol(event.target, turnChecker);
+  toggleSymbol(event.target, turn);
+  checkWin();
 }
-
 
 // INITIALIZE
 window.onload = () => {
