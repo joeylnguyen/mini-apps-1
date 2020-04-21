df2a27e2d525a687263c018a64b97bfd1193e1be
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-20 21:21:55 -0700

    Add board data that gets filled on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,76 +1,70 @@
-const cells = document.getElementsByTagName('td');
-
-
+// MODEL
 let turnChecker = false;
 
+let player1Score = 0;
+let player2Score = 0;
+let tieScore = 0;
+
+let boardData = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+const fillBoardData = (row, column, value) => {
+  console.log(boardData);
+  boardData[row][column] = value;
+}
+
+//VIEW
+
+const rows = document.getElementsByTagName('tr');
+const playAgainButton = document.getElementById('playAgain');
+
+playAgainButton.addEventListener('click', (e) => handlePlayAgainButtonClick(e));
+
 const createNewBoard = () => {
-  for (var i = 0; i < cells.length; i++) {
-    console.log(cells[i].innerHTML);
-    cells[i].style.fontSize = '50px';
-    cells[i].style.width = '85px';
-    cells[i].style.height = '85px';
-    cells[i].style.textAlign = 'center';
-    cells[i].addEventListener('click', (e) => clickHandler(e))
+  for (var i = 0; i < rows.length; i++) {
+    let cells = rows[i].children;
+    for (var j = 0; j < cells.length; j++) {
+      cells[j].style.fontSize = '50px';
+      cells[j].style.width = '85px';
+      cells[j].style.height = '85px';
+      cells[j].style.textAlign = 'center';
+      cells[j].value = {row: i, column: j};
+      cells[j].addEventListener('click', (e) => {handleCellClick(e)})
+    }
   };
 }
 
-const clickHandler = (event) => {
-  turnChecker = !turnChecker;
-  toggleSymbol(event.target, turnChecker);
+const clearBoard = () => {
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerHTML = "";
+  }
 }
 
 const toggleSymbol = (cell, turn) => (turn ? cell.innerHTML = 'X' : cell.innerHTML = 'O')
 
-window.onload = () => {
-    createNewBoard();
+// CONTROLLER
+const handlePlayAgainButtonClick = (event) => {
+  clearBoard();
+  turnChecker = false;
+
 }
 
-// class Board {
-//   constructor() {
-//     this.boardValues = [
-//       [null, null, null],
-//       [null, null, null],
-//       [null, null, null]
-//     ];
-//     this.table = document.createElement('table');
-//     this.row = document.createElement('tr');
-//     this.cell = document.createElement('td');
-//   }
+const handleCellClick = (event) => {
+  let {row, column} = event.target.value;
+
+  turnChecker = !turnChecker;
+  let value;
+  turnChecker ? value = 1 : value = 0;
+
+  fillBoardData(row, column, value);
+  toggleSymbol(event.target, turnChecker);
+}
 
 
-
-// }
-
-
-// let table = document.createElement('table');
-// let body = document.getElementById('root');
-
-
-// // Add buttons to table
-// let cell = document.createElement('td');
-
-
-
-
-/* VIEW */
-// Render Table to DOM
-// const createNewBoard = () => {
-//   let body = document.getElementById('root');
-//   let table = document.getElementsByTagName('table');
-
-//   clearBoard(body);
-
-//   let newBoard = new Board();
-//   body.appendChild(newBoard.table);
-//   console.log('New board created!');
-// }
-
-// const clearBoard = (board) => {
-//   console.log('Clearing board');
-//   board.innerHTML = "";
-// }
-
-// //INITIALIZE
-// window.onload = () => {
-//   createNewBoard();
-// }
+// INITIALIZE
+window.onload = () => {
+  createNewBoard();
+}
