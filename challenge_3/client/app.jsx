f1f03f3accd123d12b22577f7614471c2980e9f5
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-22 13:12:33 -0700

    Render text onto DOM
    - Changed babel script to compile only client files and set the
    our directory to public
    - Added express.static to serve index.html and app.js from public
    directory
    - Added crossorigin src reference to unpkg.com to allow for React
    and react-dom usage

---
+++ challenge_3/client/app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>HELLO WORLD!</h1>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 3000;
 
 // MIDDLEWARE
+app.use(express.static('public'));
 app.use((req, res, next) => {
   console.log(`Incoming ${req.method} request to ${req.path}`);
   console.log(`Body: ${req.body}`);
